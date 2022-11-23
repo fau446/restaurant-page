8953da37fd82040c0a92a26de840c6e65d2eeae8
Franklin <Franklinau2@gmail.com>
2022-11-23 12:24:49 -0800

    Add CSS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import pageLoad from './functions/pageLoad.js'
 import homeContent from './home.js'
 import menuContent from './menu.js'
 import contactContent from './contact.js'
+import './style.css'
 
 initialLoad()
 
