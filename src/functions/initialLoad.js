4a20fd483337177a4c6dfc4ca7598e1bdf5131b3
Franklin <Franklinau2@gmail.com>
2022-11-23 12:05:11 -0800

    Add createNav function

--- src/functions/initialLoad.js
+++ src/functions/initialLoad.js
@@ -1,7 +1,24 @@
 import homeContent from '../home.js'
-import pageLoad from './functions/pageLoad.js'
+import pageLoad from './pageLoad.js'
 
 export default function initialLoad() {
-
+  createNav()
   pageLoad(homeContent)
+}
+
+function createNav() {
+  let nav = document.createElement('nav')
+  let content = document.querySelector('#content')
+  let homeButton = document.createElement('button')
+  let menuButton = document.createElement('button')
+  let contactButton = document.createElement('button')
+
+  homeButton.innerHTML = "Home"
+  menuButton.innerHTML = "Menu"
+  contactButton.innerHTML = "Contact Us"
+
+  content.parentNode.insertBefore(nav, content)
+  nav.appendChild(homeButton)
+  nav.appendChild(menuButton)
+  nav.appendChild(contactButton)
 }
