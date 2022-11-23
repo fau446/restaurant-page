2ea62db70b5a81a9af09bb1bd4bee3d9a547759f
Franklin <Franklinau2@gmail.com>
2022-11-23 12:14:34 -0800

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+import initialLoad from './functions/initialLoad.js'
 import pageLoad from './functions/pageLoad.js'
 import homeContent from './home.js'
 import menuContent from './menu.js'
 import contactContent from './contact.js'
 
-pageLoad(homeContent)
+initialLoad()
+
+//cache DOM
+let homeButton = document.querySelector('.home-btn')
+let menuButton = document.querySelector('.menu-btn')
+let contactButton = document.querySelector('.contact-btn')
+
+homeButton.addEventListener('click', () => pageLoad(homeContent))
+menuButton.addEventListener('click', () => pageLoad(menuContent))
+contactButton.addEventListener('click', () => pageLoad(contactContent))
