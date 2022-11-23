55101892f2686adb28922a036115e314e94020d1
Franklin <Franklinau2@gmail.com>
2022-11-23 11:22:37 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import pageLoad from './functions/pageLoad.js'
+import homeContent from './home.js'
+import menuContent from './menu.js'
+import contactContent from './contact.js'
+
+pageLoad(homeContent)
