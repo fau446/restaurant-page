4ac387468da32bde71de466149a3ee9b2d9ab2d8
Franklin <Franklinau2@gmail.com>
2022-11-28 11:49:04 -0800

    Add contact content and layout

--- src/contact.js
+++ src/contact.js
@@ -6,5 +6,23 @@ export default function contactContent() {
   pageTitle.innerHTML = "Contact Us"
 
   content.appendChild(pageTitle)
+  content.appendChild(createContactItem('Address', '1234 Fake St, Fake City'))
+  content.appendChild(createContactItem('Phone', '(123) 456-7890'))
+  content.appendChild(createContactItem('Email', 'fakeemail@domain.com'))
   contactButton.classList.add('selected')
+}
+
+function createContactItem(headerName, content) {
+  let contactItem = document.createElement('div')
+  let header = document.createElement('h3')
+  header.innerHTML = headerName
+  let info = document.createElement('p')
+  info.innerHTML = content
+
+  contactItem.appendChild(header)
+  contactItem.appendChild(info)
+
+  contactItem.classList.add('contact-item')
+
+  return contactItem
 }
