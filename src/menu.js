73480fbc151695c0323235f2d38eab670fa3592b
Franklin <Franklinau2@gmail.com>
2022-11-28 11:28:37 -0800

    Add menu content and layout

--- src/menu.js
+++ src/menu.js
@@ -6,6 +6,35 @@ export default function menuContent() {
   pageTitle.innerHTML = "Menu"
 
   content.appendChild(pageTitle)
+  content.appendChild(createFoodItem('Bear Burger', 'The world-famous Bear Burger.', 'bear_burger.jpg'))
+  content.appendChild(createFoodItem('Mushroom Pizza', 'A delicious mushroom pizza.', 'pizza.jpg'))
 
   menuButton.classList.add('selected')
+}
+
+function createFoodItem(name, description, img_name) {
+  //cache DOM
+  let itemContainer = document.createElement('div')
+  let itemName = document.createElement('h3')
+  let itemContent = document.createElement('div')
+  let itemImage = document.createElement('img')
+  let itemDescription = document.createElement('p')
+
+  itemImage.src = `../src/images/${img_name}`
+  itemImage.width = '320'
+  itemImage.height = '180'
+
+  itemContent.classList.add('item-content')
+
+  itemContainer.appendChild(itemName)
+  itemContainer.appendChild(itemContent)
+
+  itemContent.appendChild(itemImage)
+  itemContent.appendChild(itemDescription)
+
+  itemName.innerHTML = name
+  itemDescription.innerHTML = description
+
+  itemContainer.classList.add('item')
+  return itemContainer
 }
